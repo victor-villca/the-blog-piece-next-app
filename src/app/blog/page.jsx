1d515182cc8bb21f7e95847e95e9086dccb98387
victor-villca <villcavictorleon@gmail.com>
2023-09-30 17:30:45 -0400

    Portafolio Paga UI

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,67 @@
 import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
+    <div className={styles.container}>
+      <Link href={"/blog/blogId"} className={styles.blogContainer}>
+        <div className={styles.imgContainer}>
+            <Image 
+            className={styles.img}
+            fill={true}
+            src={"https://images.pexels.com/photos/18497824/pexels-photo-18497824/free-photo-of-fashion-people-woman-summer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+            alt='image'
+            />
+        </div>
+        <div className={styles.textContainer}>
+            <h1 className={styles.title}>Title</h1>
+            <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href={"/blog/blogId"} className={styles.blogContainer}>
+        <div className={styles.imgContainer}>
+            <Image 
+            className={styles.img}
+            fill={true}
+            src={"https://images.pexels.com/photos/18497824/pexels-photo-18497824/free-photo-of-fashion-people-woman-summer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+            alt='image'
+            />
+        </div>
+        <div className={styles.textContainer}>
+            <h1 className={styles.title}>Title</h1>
+            <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href={"/blog/blogId"} className={styles.blogContainer}>
+        <div className={styles.imgContainer}>
+            <Image 
+            className={styles.img}
+            fill={true}
+            src={"https://images.pexels.com/photos/18497824/pexels-photo-18497824/free-photo-of-fashion-people-woman-summer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+            alt='image'
+            />
+        </div>
+        <div className={styles.textContainer}>
+            <h1 className={styles.title}>Title</h1>
+            <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href={"/blog/blogId"} className={styles.blogContainer}>
+        <div className={styles.imgContainer}>
+            <Image 
+            className={styles.img}
+            fill={true}
+            src={"https://images.pexels.com/photos/18497824/pexels-photo-18497824/free-photo-of-fashion-people-woman-summer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+            alt='image'
+            />
+        </div>
+        <div className={styles.textContainer}>
+            <h1 className={styles.title}>Title</h1>
+            <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>      
+    </div>
   )
 }
 
