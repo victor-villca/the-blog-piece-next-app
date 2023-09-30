3f004e843b615a4e2dcee91ae9cc6055a6b970fd
victor-villca <villcavictorleon@gmail.com>
2023-09-30 10:42:58 -0400

    Portafolio Gallery Page UI

--- src/app/portafolio/page.jsx
+++ src/app/portafolio/page.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
 const Portafolio = () => {
   return (
-    <div className={styles.container}>Portafolio</div>
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose your gallery</h1>
+      <div className={styles.items}>
+        <Link href={"/portafolio/illustrations"} className={styles.item}>
+            <span className={styles.itemText}>Ilustraitions</span>
+        </Link>
+        <Link href={"/portafolio/websites"} className={styles.item}>
+            <span className={styles.itemText}>Websites</span>
+        </Link>
+        <Link href={"/portafolio/application"} className={styles.item}>
+            <span className={styles.itemText}>Application</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
