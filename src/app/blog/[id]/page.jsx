65fa64137b103cf388ba7c577890edc614b8b36c
victor-villca <villcavictorleon@gmail.com>
2023-10-15 16:24:05 -0400

    blog fetching blog and pexel api

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
 import styles from './page.module.css'
 import Image from 'next/image'
-const BlogIdPost = () => {
+import { createClient } from 'pexels';
+async function getPhotoId(id) {
+  const client = createClient('BHcip3yhA3H3l3zet6335GbGYIQLPvDWvhmUgJniQ398RmzjAxnXbBoZ');
+  const photoId = parseInt(id.split('-')[2]);
+  const photo = await client.photos.show({ id: photoId });
+  if (!photo) {
+    throw new Error('Failed to fetch photo data');
+  }
+  
+  return photo;
+}
+
+async function getJsonPlaceholderData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id.split('-')[1]}`, {
+    cache: 'no-store'
+  });
+  if (!res.ok) {
+    throw new Error('Failed to fetch blog data' + `https://jsonplaceholder.typicode.com/posts/${id}`);
+  }
+  return res.json();
+}
+
+const BlogIdPost = async ({params}) => {
+  const placeholderData = await getJsonPlaceholderData(params.id)
+  const photoPexels = await getPhotoId(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.contentContainer}>
-          <h1 className={styles.title}>Title Blog</h1>
-          <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat culpa animi aspernatur repellendus, assumenda fugit.</p>
+          <h1 className={styles.title}>{placeholderData.title}</h1>
+          <p className={styles.desc}>{placeholderData.body}</p>
           <div className={styles.authorContainer}>
             <Image 
             className ={styles.imgAuthor}
@@ -17,27 +40,28 @@ const BlogIdPost = () => {
             alt='author img'
             />
             <p className={styles.author}>
-              Martin Anthony Lutero
+              {photoPexels.photographer}
             </p>
           </div>
 
         </div>
         <div className={styles.imgContainer}>
           <Image
-          src={"https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&w=1600"}
+          src={photoPexels.src.original}
           className={styles.imgBig}
-          fill= {true}
-          alt='big image'
+          width={550}
+          height={300}
+          alt={photoPexels.alt}
           />
         </div>
       </div>
       <div className={styles.bottom}>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo necessitatibus et dolore natus quibusdam magni, neque provident, quas nulla eius ab tempore hic! Ad repellat deserunt similique minima officiis molestias, aliquid itaque! Aliquid velit aut qui! Architecto deleniti magni consequuntur?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis non vel illo doloremque autem consequuntur sunt ab mollitia totam accusantium.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nihil repellendus neque assumenda iure voluptate ea magnam, magni consequatur animi similique earum suscipit id vel.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo necessitatibus et dolore natus quibusdam magni, neque provident, quas nulla eius ab tempore hic! Ad repellat deserunt similique minima officiis molestias, aliquid itaque! Aliquid velit aut qui! Architecto deleniti magni consequuntur?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis non vel illo doloremque autem consequuntur sunt ab mollitia totam accusantium.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nihil repellendus neque assumenda iure voluptate ea magnam, magni consequatur animi similique earum suscipit id vel.</p>
+        <p>{placeholderData.body}</p>
+        <p>{placeholderData.body}</p>
+        <p>{placeholderData.body}</p>
+        <p>{placeholderData.body}</p>
+        <p>{placeholderData.body}</p>
+        <p>{placeholderData.body}</p>
       </div>
     </div>
   )
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,66 +1,66 @@
-import React from 'react'
+'use client'
+import { useState, useEffect } from 'react'
 import styles from './page.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
-const Blog = () => {
+import { createClient } from 'pexels';
+
+
+async function getCuratedPhotos(count) {
+  const client = createClient('BHcip3yhA3H3l3zet6335GbGYIQLPvDWvhmUgJniQ398RmzjAxnXbBoZ');
+  const response = await client.photos.curated({ per_page: count });
+  return response.photos;
+}
+async function getJsonPlaceholderData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    cache: 'no-store'
+  });
+  if (!res.ok) {
+    throw new Error('Failed to fetch blog data');
+  }
+  return res.json();
+}
+
+const Blog = async () => {
+  const [photos, setPhotos] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const numberOfItems = 15;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const curatedPhotos = await getCuratedPhotos(numberOfItems);
+        setPhotos(curatedPhotos);
+
+        const jsonData = await getJsonPlaceholderData();
+        setPosts(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div className={styles.container}>
-      <Link href={"/blog/blogId"} className={styles.blogContainer}>
-        <div className={styles.imgContainer}>
-            <Image 
-            className={styles.img}
-            fill={true}
-            src={"https://images.pexels.com/photos/18497824/pexels-photo-18497824/free-photo-of-fashion-people-woman-summer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-            alt='image'
-            />
-        </div>
-        <div className={styles.textContainer}>
-            <h1 className={styles.title}>Title</h1>
-            <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href={"/blog/blogId"} className={styles.blogContainer}>
-        <div className={styles.imgContainer}>
-            <Image 
-            className={styles.img}
-            fill={true}
-            src={"https://images.pexels.com/photos/18497824/pexels-photo-18497824/free-photo-of-fashion-people-woman-summer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-            alt='image'
-            />
-        </div>
-        <div className={styles.textContainer}>
-            <h1 className={styles.title}>Title</h1>
-            <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href={"/blog/blogId"} className={styles.blogContainer}>
-        <div className={styles.imgContainer}>
-            <Image 
-            className={styles.img}
-            fill={true}
-            src={"https://images.pexels.com/photos/18497824/pexels-photo-18497824/free-photo-of-fashion-people-woman-summer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-            alt='image'
-            />
-        </div>
-        <div className={styles.textContainer}>
-            <h1 className={styles.title}>Title</h1>
-            <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href={"/blog/blogId"} className={styles.blogContainer}>
-        <div className={styles.imgContainer}>
-            <Image 
-            className={styles.img}
-            fill={true}
-            src={"https://images.pexels.com/photos/18497824/pexels-photo-18497824/free-photo-of-fashion-people-woman-summer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-            alt='image'
-            />
-        </div>
-        <div className={styles.textContainer}>
-            <h1 className={styles.title}>Title</h1>
-            <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>      
+        {photos.map((photo, index) => (
+          <Link href={`/blog/blog-${index+1}-${photo.id}`} key={index} className={styles.blogContainer}>
+              <div className={styles.imgContainer}>
+              <Image
+                className={styles.img}
+                width={400}
+                height={250}
+                src={photo.src.large}
+                alt={photo.alt}
+              />
+            </div>
+            <div className={styles.textContainer}>
+              <h1 className={styles.title}>{posts[index]?.title}</h1>
+              <p className={styles.desc}>{posts[index]?.body}</p>
+            </div>
+          </Link> 
+      )
+      )}
     </div>
   )
 }
