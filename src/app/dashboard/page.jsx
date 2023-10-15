4ac135d3550652e594187d25d0781c23d931f3b0
victor-villca <villcavictorleon@gmail.com>
2023-10-15 17:09:27 -0400

    client fetching with useEffect and swr

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+'use client'
+import {useState, useEffect} from 'react'
 import styles from './page.module.css'
+import useSWR from 'swr'
+
 const Dashboard = () => {
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/posts", fetcher)
+  console.log(data)
+
   return (
     <div className={styles.container}>Dashboard</div>
   )
 }
 
 export default Dashboard
+
+/*
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    const getData = async () =>{
+      setLoading(true)
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts",
+      {cache: 'no-store'})
+      if(!res.ok){
+        setError(true)
+        throw new Error("Something went wrong")
+      }
+      const data = await res.json()
+
+      setData(data)
+      setLoading(false)
+    }
+    getData()
+  }, [])
+
+  console.log(data)*/
