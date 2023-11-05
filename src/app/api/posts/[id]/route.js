4771d9308f215c8c42463739c17f6a7c96fe8945
victor-villca <villcavictorleon@gmail.com>
2023-11-05 09:16:33 -0400

    Dashboard ui and logic implementation

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -14,3 +14,16 @@ export const GET = async (req, {params}) => {
         return new NextResponse("Database Error",  {status: 500})
     }
 }
+
+export const DELETE = async (req, {params}) => {
+    const {id} = params;
+    try {
+        await connect();
+
+        const post = await Post.findByIdAndDelete(id);
+
+        return new NextResponse("Post was deleted", {status: 200});
+    } catch (error) {
+        return new NextResponse("Database Error",  {status: 500})
+    }
+}
