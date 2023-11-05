e78ab52a49ed999994308659048d400f4ded4c8c
victor-villca <villcavictorleon@gmail.com>
2023-11-04 21:01:01 -0400

    next-auth credentials log in

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -4,8 +4,20 @@ import styles from './page.module.css'
 import { signIn } from 'next-auth/react'
 
 const LogIn = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const email = e.target[0].value
+    const password = e.target[1].value
+
+    signIn("credentials", {email, password})
+  }
   return (
     <div className={styles.container}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input type="email" placeholder='email' className={styles.input} required/>
+          <input type="password" placeholder='password' className={styles.input} required/>
+          <button className={styles.button}>Log in</button>
+      </form>
       <button onClick={() => signIn("google")}>Log in with Google</button>  
     </div>
   )
