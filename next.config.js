aee7974cd1ca384011bf5324a7b07d5bddb0b780
victor-villca <villcavictorleon@gmail.com>
2023-11-16 13:54:28 -0400

    allow any external img as links for img posts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.pexels.com',],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            }
+        ]
     },
 }
 module.exports = nextConfig
